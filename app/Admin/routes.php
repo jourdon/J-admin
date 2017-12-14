49eceee68e421adda9831c37f50efc7a829cad91
jourdon <admin@qiehe.net>
2017-12-14 02:19:31 +0000

    add categoty model and controller

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -13,4 +13,6 @@ Route::group([
     $router->get('/', 'HomeController@index');
 
     $router->resource('users', UserController::class);
+
+    $router->resource('categories', CategoryController::class);
 });
